99316b4cde56d9ae32d70f9d5cbd9d56bdb41da2
qiqisan <qiqisan1@126.com>
2016-09-26 17:41:13 +0800

    add frequency

--- src/Business/TimeProcess.php
+++ src/Business/TimeProcess.php
@@ -10,6 +10,16 @@ namespace FSth\SwooleDaemonize\Business;
 abstract class TimeProcess extends Process
 {
     protected $frequency = 1000;
+    protected $processConfig;
+
+    public function __construct($name, $config)
+    {
+        parent::__construct($name, $config);
+        if (!empty($config['processes']) && !empty($config['processes'][$name])) {
+            $this->processConfig = $config['processes'][$name];
+        }
+        $this->setUp();
+    }
 
     public function setFrequency($frequency)
     {
@@ -18,9 +28,19 @@ abstract class TimeProcess extends Process
 
     protected function execute()
     {
-        \swoole_timer_tick($this->frequency, function(){
-           $this->process();
+        \swoole_timer_tick($this->frequency, function () {
+            $this->process();
         });
+    }
+
+    protected function setUp()
+    {
+        if (empty($this->processConfig)) {
+            return;
+        }
+        if (!empty($this->processConfig['frequency'])) {
+            $this->frequency = $this->processConfig['frequency'];
+        }
     }
 
     abstract protected function process();
